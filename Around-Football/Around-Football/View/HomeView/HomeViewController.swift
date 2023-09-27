98fe25a0271f74ff3998c4b71e193a4192531357
Taeyoung Jin <elisha0103@naver.com>
2023-09-27 13:53:10 +0900

    Add: ViewController
    - 추가
      - MainTabController(탭바)
      - HomeViewController
      - MapViewController
      - InfoViewController

---
+++ Around-Football/Around-Football/View/HomeView/HomeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  HomeViewController.swift
+//  Around-Football
+//
+//  Created by 진태영 on 2023/09/27.
+//
+
+import UIKit
+
+class HomeViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        configureUI()
+    }
+    
+    func configureUI() {
+        view.backgroundColor = .systemRed
+    }
+}
