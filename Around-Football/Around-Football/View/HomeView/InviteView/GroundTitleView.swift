f322fd06c83ffd55393699000ce15a83e064ba3b
thekoon0456 <thekoon0456@gmail.com>
2023-10-02 01:52:32 +0900

    [Feat] InviteView 추가

---
+++ Around-Football/Around-Football/View/HomeView/InviteView/GroundTitleView.swift
@@ -0,0 +1,47 @@
+//
+//  GroundTitleView.swift
+//  Around-Football
+//
+//  Created by Deokhun KIM on 10/2/23.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class GroundTitleView: UIView {
+    
+    private let groundTitleLabel = UILabel().then {
+        $0.text = "장소"
+        $0.font = .systemFont(ofSize: 15, weight: .bold)
+    }
+    var groundNameLabel = UILabel().then {
+        $0.text = "종합운동장"
+        $0.font = .systemFont(ofSize: 15, weight: .bold)
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureUI() {
+        addSubview(groundTitleLabel)
+        addSubview(groundNameLabel)
+        
+        groundTitleLabel.snp.makeConstraints { make in
+            make.top.leading.equalToSuperview().offset(10)
+        }
+        
+        groundNameLabel.snp.makeConstraints { make in
+            make.top.equalTo(groundTitleLabel.snp.bottom).offset(5)
+            make.leading.equalToSuperview()
+            make.bottom.equalToSuperview().offset(10)
+        }
+    }
+}
