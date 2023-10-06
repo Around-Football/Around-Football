fb06c294e6056d73acd49e4eaee353b1f11b6e4b
Changhyun Kang <changhyun.kyle@gmail.com>
2023-10-06 17:02:43 +0900

    [Feat] SignUpView Title Label 생성

--- Around-Football/Around-Football/View/SignUpView/SignUpViewController.swift
+++ Around-Football/Around-Football/View/SignUpView/SignUpViewController.swift
@@ -7,8 +7,21 @@
 
 import Foundation
 import UIKit
+import SnapKit
+import Then
 
 class SignUpViewController: UIViewController {
+    
+    private let titleLabel = UILabel().then {
+        $0.text = "회원가입"
+        $0.textColor = .black
+        // FIXME: - LargeTitle + Bold 구현
+        $0.font = .boldSystemFont(ofSize: 30)
+        
+//        $0.font = .preferredFont(forTextStyle: .largeTitle)
+        
+        //  label.font = UIFont.systemFont(ofSize: UIFont.smallSystemFontSize)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,6 +30,12 @@ class SignUpViewController: UIViewController {
     }
     
     func configureUI() {
-        view.backgroundColor = .systemRed
+        view.backgroundColor = .white
+        view.addSubview(titleLabel)
+        titleLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview()
+            make.leading.equalToSuperview().offset(20) // leading margin을 20 포인트로 설정
+        }
     }
 }
