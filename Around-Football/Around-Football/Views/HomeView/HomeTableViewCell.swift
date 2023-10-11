e039925d6d6161828ef4e551617b4f6baa130337
Changhyun Kang <changhyun.kyle@gmail.com>
2023-10-11 22:17:15 +0900

    [Add] HomeTableViewCell 파일 추가

---
+++ Around-Football/Around-Football/Views/HomeView/HomeTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  HomeTableViewCell.swift
+//  Around-Football
+//
+//  Created by 강창현 on 10/11/23.
+//
+
+import UIKit
+
+import RxSwift
+import RxCocoa
+
+class HomeTableViewCell: UITableViewCell {
+    private var titleLabel = UILabel()
+    private var fieldAddress = UILabel()
+}
