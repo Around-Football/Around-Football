313d2c0a8555b6017d836f7ed4f22843a1d563bf
Changhyun Kang <changhyun.kyle@gmail.com>
2023-10-12 14:54:16 +0900

    [Add] HomeViewModel 추가

--- Around-Football/Around-Football/Views/HomeView/HomeTableViewCell.swift
+++ Around-Football/Around-Football/Views/HomeView/HomeTableViewCell.swift
@@ -9,8 +9,20 @@ import UIKit
 
 import RxSwift
 import RxCocoa
-
+/*
+     1. Properties
+     2. Lifecycles
+     3. API
+     4. Selectors
+     5. Helpers
+ */
 class HomeTableViewCell: UITableViewCell {
+    static let id: String = "HomeTableViewCell"
+    
     private var titleLabel = UILabel()
     private var fieldAddress = UILabel()
+    // private var date = UILabel()
+    // private var time = UILabel()
+    
+    
 }
