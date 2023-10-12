9c9e37bd8fae08f3800d9b9646958bfa611b3c8c
Changhyun Kang <changhyun.kyle@gmail.com>
2023-10-12 19:30:26 +0900

    [Test] MockData 통신 함수 구현

--- Around-Football/Around-Football/Utiles/APIService.swift
+++ Around-Football/Around-Football/Utiles/APIService.swift
@@ -6,3 +6,41 @@
 //
 
 import Foundation
+import RxSwift
+
+let MOCKUP_URL = "https://mocki.io/v1/acd3f149-d024-4f71-850d-ad7eb108de50"
+
+class APIService {
+    
+    static func fetchfetchRecruitRx() -> Observable<Data> {
+        return Observable.create { emitter in
+            fetchRecruit() { result in
+                switch result {
+                case let .success(data):
+                    emitter.onNext(data)
+                    emitter.onCompleted()
+                case let .failure(error):
+                    emitter.onError(error)
+                }
+            }
+            return Disposables.create()
+        }
+    }
+    
+    static func fetchRecruit(onComplete: @escaping (Result<Data, Error>) -> Void) {
+        URLSession.shared.dataTask(with: URL(string: MOCKUP_URL)!) { data, res, err in
+            if let err = err {
+                onComplete(.failure(err))
+                return
+            }
+            guard let data = data else {
+                let httpResponse = res as! HTTPURLResponse
+                onComplete(.failure(NSError(domain: "no data",
+                                            code: httpResponse.statusCode,
+                                            userInfo: nil)))
+                return
+            }
+            onComplete(.success(data))
+        }.resume()
+    }
+}
