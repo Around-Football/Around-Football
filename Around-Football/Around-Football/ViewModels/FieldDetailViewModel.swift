83e05d387eefd6a301d03e011e0fdfb46cb0bc48
Taeyoung Jin <elisha0103@naver.com>
2023-10-13 18:21:17 +0900

    [Feat]: Present FieldDetailView
    - FieldDetailView 모달 Present 기능 구현

---
+++ Around-Football/Around-Football/ViewModels/FieldDetailViewModel.swift
@@ -0,0 +1,26 @@
+//
+//  FieldDetailViewModel.swift
+//  Around-Football
+//
+//  Created by 진태영 on 10/13/23.
+//
+
+import Foundation
+
+final class FieldDetailViewModel {
+    
+    // MARK: - Properties
+    
+    var id: String
+    
+    // MARK: - Lifecycles
+    
+    init(id: String) {
+        self.id = id
+    }
+    
+    // MARK: - API
+    
+    
+    
+}
