23d2f7f1851e0042149041b6d6f5e44d25357d36
Changhyun Kang <changhyun.kyle@gmail.com>
2023-10-18 03:03:50 +0900

    [Test] Google Login 기능 테스트 코드 작성

--- Around-Football/Around-Football/Views/LoginView/GoogleLoginService.swift
+++ Around-Football/Around-Football/Views/LoginView/GoogleLoginService.swift
@@ -6,3 +6,76 @@
 //
 
 import Foundation
+
+import FirebaseCore
+import GoogleSignIn
+import FirebaseAuth
+
+class GoogleLoginService: UIViewController {
+    
+    func googleSignIn() {
+        
+        guard let clientID = FirebaseApp.app()?.options.clientID else { return }
+        
+        // Create Google Sign In configuration object.
+        let config = GIDConfiguration(clientID: clientID)
+        GIDSignIn.sharedInstance.configuration = config
+        
+        // Start the sign in flow!
+        GIDSignIn.sharedInstance.signIn(withPresenting: self) { result, error in
+            guard error == nil else {
+                // ...
+                print("signIn ERROR: \(error?.localizedDescription)")
+                return
+            }
+            
+            guard let user = result?.user,
+                  let idToken = user.idToken?.tokenString
+            else {
+                // ...
+                print("idToken 에러")
+                return
+            }
+            print("구글 로그인 Flow 진행")
+            let credential = GoogleAuthProvider.credential(withIDToken: idToken,
+                                                           accessToken: user.accessToken.tokenString)
+            
+            Auth.auth().signIn(with: credential) { [weak self] result, error in
+                if let error = error {
+                    print ("Error Apple sign in: %@", error)
+                    return
+                }
+                
+                // 로그인 성공 시 MainTabView로 화면 이동
+                let mainTabVC = MainTabController()
+                if let navigationController = self?.navigationController {
+                    navigationController.pushViewController(mainTabVC, animated: true)
+                }   
+            }
+            
+        }
+    }
+    //    func createGoogleUser(email: String, password: String) {
+    //        Auth.auth().createUser(withEmail: email, password: password) { result, error in
+    //            guard error == nil else {
+    //                print(error?.localizedDescription as Any)
+    //                return
+    //            }
+    //
+    //            self.googleSignIn(email: email, password: password) //로그인
+    //        }
+    //    }
+    //
+    //    //구글 로그인
+    //    func googleSignIn(email: String, password: String) {
+    //        Auth.auth().signIn(withEmail: email, password: password) { result, error in
+    //            guard error == nil else {
+    //                print(error?.localizedDescription as Any)
+    //                return
+    //            }
+    //
+    //            print("로그인 성공")
+    //            print(result?.user)
+    //        }
+    //    }
+}
