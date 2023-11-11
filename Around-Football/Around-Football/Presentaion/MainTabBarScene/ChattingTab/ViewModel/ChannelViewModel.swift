d7900cf0960d0d1a312299788fa15ccae14f9ae1
Taeyoung Jin <elisha0103@naver.com>
2023-11-11 18:20:27 +0900

    [Feat]: FirebaeAPI 함수 추가
    - UpdateFCMToken And FetchUser 함수 추가
    - ChannelViewModel FetchUser 함수 정의

--- Around-Football/Around-Football/Presentaion/MainTabBarScene/ChattingTab/ViewModel/ChannelViewModel.swift
+++ Around-Football/Around-Football/Presentaion/MainTabBarScene/ChattingTab/ViewModel/ChannelViewModel.swift
@@ -17,9 +17,32 @@ final class ChannelViewModel {
     var channels: [ChannelInfo] = []
     var currentUser: User?
     
-    
     // MARK: - API
     
+    func setupListener() {
+        ChannelAPI.shared.subscribe { [weak self] result in
+            switch result {
+            case .success(let data):
+            case .failure(let error):
+                print("DEBUG - setupListener Error: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    func fetchUser() {
+        let uid = Auth.auth().currentUser?.uid ?? UUID().uuidString
+        let fcmToken = UserDefaults.standard.string(forKey: "FCMToken") ?? ""
+        FirebaseAPI.shared.updateFCMTokenAndFetchUser(uid: uid, fcmToken: fcmToken) { [weak self] user, error in
+            if let error = error {
+                print("DEBUG - FetchUser Error: ", #function, error.localizedDescription)
+                return
+            }
+            guard let self = self else { return }
+            self.currentUser = user
+            print("DEBUG - CurrentUser: \(String(describing: self.currentUser))")
+
+        }
+    }
     // MARK: - Helpers
     
 }
