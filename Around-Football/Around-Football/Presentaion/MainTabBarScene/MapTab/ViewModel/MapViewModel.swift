7b570059763b74ca68d05d42a5ef92c81e307bd5
Changhyun Kang <changhyun.kyle@gmail.com>
2023-11-13 16:16:35 +0900

    [Feat] setSearchLocation() -> RxSwift & RxAlamofire 활용하여 카카오 REST API 구현

--- Around-Football/Around-Football/Presentaion/MainTabBarScene/MapTab/ViewModel/MapViewModel.swift
+++ Around-Football/Around-Football/Presentaion/MainTabBarScene/MapTab/ViewModel/MapViewModel.swift
@@ -7,7 +7,10 @@
 
 import Foundation
 
+import Alamofire
 import FirebaseFirestore
+import RxAlamofire
+import RxSwift
 
 final class MapViewModel {
     
@@ -16,6 +19,7 @@ final class MapViewModel {
     var currentLocation: GeoPoint
     var searchLocation: GeoPoint?
     var isSearchCurrentLocation: Bool = true
+    private let disposeBag = DisposeBag()
     private let firebaseAPI = FirebaseAPI.shared
     var fields: [Field] = []
     var selectedDate: Date = Date() {
@@ -48,7 +52,41 @@ final class MapViewModel {
         currentLocation = GeoPoint(latitude: latitude, longitude: longitude)
     }
     
-    func setSearchLocation(latitude: Double, longitude: Double) {
-        searchLocation = GeoPoint(latitude: latitude, longitude: longitude)
+    func setSearchLocation(_ keyword: String) {
+        // searchLocation = GeoPoint(latitude: latitude, longitude: longitude)
+//        guard let apiUrl = URL(string: "https://dapi.kakao.com/v2/local/search/keyword.json") else {
+//            return
+//        }
+//        
+        guard let apiUrl = URL(string: "https://dapi.kakao.com/v2/local/search/keyword.json?query=\(keyword.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? "")") else {
+            return
+        }
+        
+        let apiKey = "07874a02a8d48af697b51048893c3d70"
+        let headers: HTTPHeaders = ["Authorization": "KakaoAK \(apiKey)"]
+
+        // RxAlamofire를 사용한 비동기 요청
+        RxAlamofire
+            .data(.get, apiUrl, headers: headers)
+            .observe(on: MainScheduler.instance)
+            .subscribe(onNext: { [weak self] data in
+                do {
+                    let decoder = JSONDecoder()
+                    let result = try decoder.decode(KakaoPlaceResponse.self, from: data)
+                    
+                    if let firstPlace = result.documents.first {
+                        // 검색된 장소 중 첫 번째 장소의 좌표로 이동
+//                        self?.moveToLocation(latitude: firstPlace.y, longitude: firstPlace.x)
+                        self?.searchLocation = GeoPoint(latitude: Double(firstPlace.y) ?? 0.0, longitude: Double(firstPlace.x) ?? 0.0)
+                    } else {
+                        print("장소를 찾을 수 없습니다.")
+                    }
+                } catch {
+                    print("Error decoding JSON: \(error.localizedDescription)")
+                }
+            }, onError: { error in
+                print("Error: \(error.localizedDescription)")
+            })
+            .disposed(by: disposeBag)
     }
 }
