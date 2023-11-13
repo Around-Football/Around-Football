769d100f1ef1846c78c7afd3c90e7dd6192a97eb
Changhyun Kang <changhyun.kyle@gmail.com>
2023-11-13 20:13:40 +0900

    [Refactor] 지도 검색 모델 -> SearchModel로 분기 처리

--- Around-Football/Around-Football/Models/MapModel.swift
+++ Around-Football/Around-Football/Models/MapModel.swift
@@ -66,34 +66,3 @@ enum Poi {
 //        }
 //    }
 //}
-
-struct Place: Codable {
-    let id: String
-    let name: String
-    let address: String
-    let x: String
-    let y: String
-}
-
-struct KakaoPlaceResponse: Codable {
-    let documents: [PlaceData]
-}
-
-struct PlaceData: Codable {
-    let addressName, categoryGroupCode, categoryGroupName, categoryName: String
-    let distance, id, phone, placeName: String
-    let placeURL: String
-    let roadAddressName, x, y: String
-    
-    enum CodingKeys: String, CodingKey {
-        case addressName = "address_name"
-        case categoryGroupCode = "category_group_code"
-        case categoryGroupName = "category_group_name"
-        case categoryName = "category_name"
-        case distance, id, phone
-        case placeName = "place_name"
-        case placeURL = "place_url"
-        case roadAddressName = "road_address_name"
-        case x, y
-    }
-}
