e4f5116dcfa3b29dcafb76db4c3929c21384e333
Taeyoung Jin <elisha0103@naver.com>
2023-11-15 20:52:15 +0900

    [Feat]: ChatAPI
    - ChatAPI
        - Observable<[Message]> 반환형 subscribe
        - save(_:, completion:)
        - removeListener()

---
+++ Around-Football/Around-Football/Network/ChatAPI.swift
@@ -0,0 +1,57 @@
+//
+//  ChatAPI.swift
+//  Around-Football
+//
+//  Created by 진태영 on 11/15/23.
+//
+
+import Foundation
+
+import RxSwift
+import FirebaseFirestore
+
+class ChatAPI {
+    static let shared = ChatAPI()
+    var listener: ListenerRegistration?
+    var collectionListener: CollectionReference?
+    
+    func subscribe(id: String) -> Observable<[Message]> {
+        
+        removeListenr()
+        collectionListener = REF_CHANNELS.document(id).collection("thread")
+        
+        return Observable.create { [weak self] observer in
+            let disposable = Disposables.create()
+            
+            guard let self = self else { return Disposables.create() }
+            
+            self.listener = collectionListener?
+                .addSnapshotListener({ snapshot, error in
+                    guard let snapshot = snapshot else {
+                        observer.onError(error ?? NSError(domain: "", code: -1))
+                        return
+                    }
+                    var messages: [Message] = []
+                    snapshot.documentChanges.forEach { change in
+                        if let message = Message(dictionary: change.document.data()) {
+                            if case .added = change.type {
+                                messages.append(message)
+                            }
+                        }
+                    }
+                    observer.onNext(messages)
+                })
+            return disposable
+        }
+    }
+    
+    func save(_ message: Message, completion: ((Error?) -> Void)? = nil) {
+        collectionListener?.addDocument(data: message.representation, completion: { error in
+            completion?(error)
+        })
+    }
+    
+    func removeListenr() {
+        listener?.remove()
+    }
+}
