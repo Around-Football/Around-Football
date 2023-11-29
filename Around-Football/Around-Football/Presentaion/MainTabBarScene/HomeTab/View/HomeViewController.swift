9579c0efb9d613998f4845063db835982565b266
thekoon0456 <thekoon0456@gmail.com>
2023-11-29 23:27:26 +0900

    [Refactor] 상단 스크롤뷰 버튼으로 대체

--- Around-Football/Around-Football/Presentaion/MainTabBarScene/HomeTab/View/HomeViewController.swift
+++ Around-Football/Around-Football/Presentaion/MainTabBarScene/HomeTab/View/HomeViewController.swift
@@ -34,19 +34,96 @@ final class HomeViewController: UIViewController {
         $0.register(HomeTableViewCell.self, forCellReuseIdentifier: HomeTableViewCell.id)
     }
     
-    private let filterOptions: [String] = ["모든 날짜", "모든 지역", "매치 유형"] // 필터 옵션
-    
     private lazy var filterScrollView = UIScrollView().then {
         $0.contentSize = CGSize(width: view.frame.size.width, height: 30)
         $0.showsHorizontalScrollIndicator = false
-    } // 가로 스크롤뷰
+    }
     
-    private var optionStackView = UIStackView().then {
+    private lazy var optionStackView = UIStackView().then {
         $0.axis = .horizontal
         $0.alignment = .fill
         $0.distribution = .fillEqually
         $0.spacing = 5
-        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.addArrangedSubviews(resetButton, dateFilterButton, areaFilterButton, typeFilterButton)
+    }
+    
+    private var buttonConfig: UIButton.Configuration {
+        var config = UIButton.Configuration.plain()
+        config.imagePadding = 8
+        config.contentInsets = NSDirectionalEdgeInsets(top: 10,
+                                                       leading: 10,
+                                                       bottom: 10,
+                                                       trailing: 10)
+        
+        return config
+    }
+    
+    private lazy var resetButton = UIButton(configuration: buttonConfig).then {
+        let image = UIImage(systemName: "arrow.triangle.2.circlepath")
+        $0.setTitle("초기화", for: .normal)
+        $0.setImage(image?.withTintColor(UIColor.systemGray, renderingMode: .alwaysOriginal),
+                        for: .normal)
+        $0.setTitleColor(.systemGray, for: .normal)
+        $0.layer.cornerRadius = LayoutOptions.cornerRadious // 버튼의 모서리를 둥글게 만듭니다.
+        $0.layer.borderWidth = 1.0
+        $0.layer.borderColor = UIColor.systemGray.cgColor
+        $0.addTarget(self, action: #selector(resetFilterButtonTapped), for: .touchUpInside)
+    }
+    
+    private lazy var dateFilterButton = UIButton(configuration: buttonConfig).then {
+        let image = UIImage(systemName: "chevron.down")
+        $0.setTitle("날짜 선택", for: .normal)
+        $0.setImage(image?.withTintColor(UIColor.systemGray, renderingMode: .alwaysOriginal),
+                        for: .normal)
+        $0.setTitleColor(.systemGray, for: .normal)
+        $0.layer.cornerRadius = LayoutOptions.cornerRadious
+        $0.layer.borderWidth = 1.0
+        $0.layer.borderColor = UIColor.systemGray.cgColor
+        $0.addTarget(self, action: #selector(dateFilterButtonTapped), for: .touchUpInside)
+    }
+    
+    private lazy var areaFilterButton = UIButton(configuration: buttonConfig).then {
+        let image = UIImage(systemName: "chevron.down")
+        $0.setTitle("지역 선택", for: .normal)
+        $0.setImage(image?.withTintColor(UIColor.systemGray, renderingMode: .alwaysOriginal),
+                        for: .normal)
+        $0.setTitleColor(.systemGray, for: .normal)
+        $0.layer.cornerRadius = LayoutOptions.cornerRadious
+        $0.layer.borderWidth = 1.0
+        $0.layer.borderColor = UIColor.systemGray.cgColor
+        $0.addTarget(self, action: #selector(areaFilterButtonTapped), for: .touchUpInside)
+    }
+    
+    private lazy var typeFilterButton = UIButton(configuration: buttonConfig).then {
+        let image = UIImage(systemName: "chevron.down")
+        $0.setTitle("매치 유형", for: .normal)
+        $0.setImage(image?.withTintColor(UIColor.systemGray, renderingMode: .alwaysOriginal),
+                        for: .normal)
+        $0.setTitleColor(.systemGray, for: .normal)
+        $0.layer.cornerRadius = LayoutOptions.cornerRadious
+        $0.layer.borderWidth = 1.0
+        $0.layer.borderColor = UIColor.systemGray.cgColor
+        $0.addTarget(self, action: #selector(typeFilterButtonTapped), for: .touchUpInside)
+    }
+    
+    @objc
+    func resetFilterButtonTapped() {
+        
+    }
+    
+    @objc
+    func dateFilterButtonTapped() {
+        
+    }
+    
+    @objc
+    func areaFilterButtonTapped() {
+        
+    }
+    
+    @objc
+    func typeFilterButtonTapped() {
+        
     }
     
     private lazy var floatingButton: UIButton = {
@@ -65,37 +142,6 @@ final class HomeViewController: UIViewController {
         return button
     }()
     
-    private var buttonConfig: UIButton.Configuration {
-        var config = UIButton.Configuration.plain()
-        config.contentInsets = NSDirectionalEdgeInsets(top: 10,
-                                                       leading: 10,
-                                                       bottom: 10,
-                                                       trailing: 10)
-        
-        return config
-    }
-    
-    private lazy var resetButton: UIButton = {
-        
-        let button = UIButton(configuration: buttonConfig)
-        let image = UIImage(systemName: "arrow.triangle.2.circlepath")
-        // 버튼 타이틀과 이미지 설정
-        button.setTitle("초기화", for: .normal)
-        button.setImage(image?.withTintColor(UIColor.systemGray, renderingMode: .alwaysOriginal),
-                        for: .normal)
-        
-        // 버튼 스타일 설정
-        button.setTitleColor(.systemGray, for: .normal)
-        button.layer.cornerRadius = LayoutOptions.cornerRadious // 버튼의 모서리를 둥글게 만듭니다.
-        button.layer.borderWidth = 1.0
-        button.layer.borderColor = UIColor.systemGray.cgColor
-        
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.addTarget(self, action: #selector(filterOptionTapped), for: .touchUpInside)
-        
-        return button
-    }()
-    
     // MARK: - Lifecycles
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -108,7 +154,7 @@ final class HomeViewController: UIViewController {
         navigationController?.navigationBar.isHidden = true
         //        invokedViewWillAppear.onNext(())
         filterringRegionRecruitList.onNext(locationFilterView.selectedCity)
-        print(locationFilterView.selectedCity)
+//        print(locationFilterView.selectedCity)
     }
     
     override func viewDidLayoutSubviews() {
@@ -157,39 +203,11 @@ final class HomeViewController: UIViewController {
     
     func configureUI() {
         view.backgroundColor = .white
-        
-        optionStackView.addArrangedSubview(resetButton)
-        
-        for option in filterOptions {
-            let filterButton: UIButton = {
-                let button = UIButton(configuration: buttonConfig)
-                let image = UIImage(systemName: "chevron.down")
-                // 버튼 타이틀과 이미지 설정
-                button.setTitle(option, for: .normal)
-                button.setImage(
-                    image?.withTintColor(UIColor.systemGray, renderingMode: .alwaysOriginal),
-                    for: .normal)
-                
-                // 버튼 스타일 설정
-                button.setTitleColor(.systemGray, for: .normal)
-                button.layer.cornerRadius = LayoutOptions.cornerRadious
-                button.layer.borderWidth = 1.0
-                button.layer.borderColor = UIColor.systemGray.cgColor
-                
-                button.translatesAutoresizingMaskIntoConstraints = false
-                button.addTarget(self, action: #selector(filterOptionTapped), for: .touchUpInside)
-                return button
-            }()
-            
-            optionStackView.addArrangedSubview(filterButton)
-        }
-        
-        filterScrollView.addSubview(optionStackView)
-        
         view.addSubviews(filterScrollView,
                          homeTableView,
                          floatingButton)
         
+        filterScrollView.addSubview(optionStackView)
         filterScrollView.snp.makeConstraints { make in
             make.top.equalTo(view.safeAreaLayoutGuide).offset(SuperviewOffsets.topPadding)
             make.leading.equalToSuperview().offset(SuperviewOffsets.leadingPadding)
@@ -212,61 +230,103 @@ final class HomeViewController: UIViewController {
         }
     }
     
-    //TODO: - 방식 맞추기 (모달 or alertsheet)
+//    //TODO: - 방식 맞추기 (모달 or alertsheet)
+//    
+//    func matchTypeActionSheet() -> UIAlertController {
+//        let actionSheet = UIAlertController()
+//        
+//        actionSheet.title = "매치 유형을 선택해주세요"
+//        
+//        //전체 버튼 - 스타일(default)
+//        actionSheet.addAction(UIAlertAction(title: "전체", style: .default, handler: {[weak self] (ACTION:UIAlertAction) in
+//            print("전체")
+//            self?.filteringTypeRecruitList.onNext(nil)
+//        }))
+//        
+//        // 축구
+//        actionSheet.addAction(UIAlertAction(title: "축구", style: .default, handler: { [weak self] _ in
+//            print("축구")
+//            self?.filteringTypeRecruitList.onNext("축구")
+//        }))
+//        
+//        // 풋살
+//        actionSheet.addAction(UIAlertAction(title: "풋살", style: .default, handler: { [weak self] _ in
+//            print("풋살")
+//            self?.filteringTypeRecruitList.onNext("풋살")
+//        }))
+//        
+//        //취소 버튼 - 스타일(cancel)
+//        actionSheet.addAction(UIAlertAction(title: "취소", style: .destructive, handler: nil))
+//        
+//        return actionSheet
+//    }
+
     
-    func matchTypeActionSheet() -> UIAlertController {
-        let actionSheet = UIAlertController()
-        
-        actionSheet.title = "매치 유형을 선택해주세요"
-        
-        //전체 버튼 - 스타일(default)
-        actionSheet.addAction(UIAlertAction(title: "전체", style: .default, handler: {[weak self] (ACTION:UIAlertAction) in
-            print("전체")
-            self?.filteringTypeRecruitList.onNext(nil)
-        }))
-        
-        // 축구
-        actionSheet.addAction(UIAlertAction(title: "축구", style: .default, handler: { [weak self] _ in
-            print("축구")
-            self?.filteringTypeRecruitList.onNext("축구")
-        }))
-        
-        // 풋살
-        actionSheet.addAction(UIAlertAction(title: "풋살", style: .default, handler: { [weak self] _ in
-            print("풋살")
-            self?.filteringTypeRecruitList.onNext("풋살")
-        }))
-        
-        //취소 버튼 - 스타일(cancel)
-        actionSheet.addAction(UIAlertAction(title: "취소", style: .destructive, handler: nil))
-        
-        return actionSheet
-    }
+//    let typeItems = ["전체, 축구, 풋살"]
+//    
+//    private lazy var pickerView: UIPickerView = {
+//        
+//        let pickerView = UIPickerView()
+//        
+//        pickerView.delegate = self
+//        pickerView.dataSource = self
+//        return pickerView
+//    }()
+//    
+//    private lazy var textField: UITextField = {
+//        let textfield = UITextField()
+//        textfield.inputView = pickerView
+//        return textfield
+//    }()
+
+//    
+//    private let typePickerView = UIPickerView()
     
-    // MARK: - Selectors
+//    private func setupTypePicker() {
+//        typePickerView.delegate = self
+//        
+//        Observable
+//            .combineLatest(typePickerView.rx.itemSelected, typePickerView.rx.modelSelected(String.self))
+//            .map { _, item in
+//                return item.first ?? ""
+//            }
+////            .bind(to: textField.rx.text)
+//            .disposed(by: disposeBag)
+//        
+//        // RxSwift와 RxCocoa를 사용하여 피커 뷰의 데이터 소스를 설정
+//        Observable
+//            .just(typeItems)
+//            .bind(to: typePickerView.rx.itemTitles) { _, item in
+//                return item
+//            }
+//            .disposed(by: disposeBag)
+//    }
     
-    @objc
-    func filterOptionTapped(sender: UIButton) {
-        // TODO: - Coordinator로 변경
-        // 필터 옵션 버튼을 탭했을 때의 동작
-        // ["모든 날짜", "모든 지역", "매치 유형"]
-        
-        switch sender.title(for: .normal) {
-        case "모든 날짜":
-            present(dateFilterView, animated: true)
-            
-        case "모든 지역":
-            locationFilterView.modalPresentationStyle = .fullScreen
-            locationFilterView.modalTransitionStyle = .coverVertical
-            present(locationFilterView, animated: true)
-            
-        case "매치 유형":
-            self.present(matchTypeActionSheet(), animated: true, completion: nil)
-            
-        default:
-            break
-        }
-    }
+//    // MARK: - Selectors
+//    
+//    @objc
+//    func filterOptionTapped(sender: UIButton) {
+//        // TODO: - Coordinator로 변경
+//        // 필터 옵션 버튼을 탭했을 때의 동작
+//        // ["모든 날짜", "모든 지역", "매치 유형"]
+//        
+//        switch sender.title(for: .normal) {
+//        case "모든 날짜":
+//            present(dateFilterView, animated: true)
+//            
+//        case "모든 지역":
+//            locationFilterView.modalPresentationStyle = .fullScreen
+//            locationFilterView.modalTransitionStyle = .coverVertical
+//            present(locationFilterView, animated: true)
+//            
+//        case "매치 유형":
+//            self.present(matchTypeActionSheet(), animated: true, completion: nil)
+//            
+//
+//        default:
+//            break
+//        }
+//    }
     
     // FIXME: - View PopUp navigationBar 처리
     @objc
@@ -282,3 +342,22 @@ final class HomeViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
 }
+
+//extension HomeViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+//    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+//          return 1
+//      }
+//      
+//      func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+//          return typeItems.count
+//      }
+//      
+//      // UIPickerViewDelegate 메서드
+//      func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+//          return typeItems[row]
+//      }
+//      
+//      func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+//          textField.text = typeItems[row]
+//      }
+//}
