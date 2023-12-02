61d6038211dccb0364098a403be6ec3c77f26123
thekoon0456 <thekoon0456@gmail.com>
2023-12-02 19:39:11 +0900

    [Fix] AgeTextfield numberPad로 변경

--- Around-Football/Around-Football/Presentaion/InputInfoScene/View/InputInfoView/InputInfoViewController+.swift
+++ Around-Football/Around-Football/Presentaion/InputInfoScene/View/InputInfoView/InputInfoViewController+.swift
@@ -27,7 +27,6 @@ extension InputInfoViewController: UITextFieldDelegate {
         return true
     }
     
-    
     // 엔터 누르면 다음 텍스트필드로 넘어감
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         if textField == inputInfoView.userNameTextField {
@@ -38,29 +37,30 @@ extension InputInfoViewController: UITextFieldDelegate {
         return true
     }
     
-    // 지역 입력 텍스트필드 선택 시 뷰 이동 (위로 올림)
-    func textFieldDidBeginEditing(_ textField: UITextField) {
-        if textField == inputInfoView.userAreaTextField {
-            UIView.animate(withDuration: 0.3) {
-                let transform = CGAffineTransform(translationX: 0, y: -200)
-                self.view.transform = transform
-            }
-        }
-    }
-    
-    // 뷰 다시 원위치
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        if textField == inputInfoView.userAreaTextField {
-            UIView.animate(withDuration: 0.3) {
-                let transform = CGAffineTransform(translationX: 0, y: 0)
-                self.view.transform = transform
-            }
-        }
-    }
-    
-    @objc 
+    //화면 누르면 키보드 내려감
+    @objc
     func dismissKeyboard() {
         view.endEditing(true)
     }
+    
+//    // 지역 입력 텍스트필드 선택 시 뷰 이동 (위로 올림)
+//    func textFieldDidBeginEditing(_ textField: UITextField) {
+//        if textField == inputInfoView.userAreaTextField {
+//            UIView.animate(withDuration: 0.3) {
+//                let transform = CGAffineTransform(translationX: 0, y: -200)
+//                self.view.transform = transform
+//            }
+//        }
+//    }
+    
+//    // 뷰 다시 원위치
+//    func textFieldDidEndEditing(_ textField: UITextField) {
+//        if textField == inputInfoView.userAreaTextField {
+//            UIView.animate(withDuration: 0.3) {
+//                let transform = CGAffineTransform(translationX: 0, y: 0)
+//                self.view.transform = transform
+//            }
+//        }
+//    }
 }
 
