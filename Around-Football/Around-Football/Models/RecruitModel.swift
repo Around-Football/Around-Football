597dc7e938257739f74c30c05e01e65424205d3b
thekoon0456 <thekoon0456@gmail.com>
2023-12-05 16:03:52 +0900

    [Feat] applicant 콜렉션 생성, 삭제 구현

--- Around-Football/Around-Football/Models/RecruitModel.swift
+++ Around-Football/Around-Football/Models/RecruitModel.swift
@@ -24,6 +24,7 @@ struct Recruit: Codable, Identifiable {
     var matchDateString: String? //날짜만, String으로 일단 수정
     var startTime: String? //시작시간
     var endTime: String? // 종료시간
+    var acceptedApplicants: [String] = []
     
     //TODO: - 서브 컬렉션으로 신청한사람 Uid, 수락했는지 여부 추가하기
     
@@ -31,12 +32,46 @@ struct Recruit: Codable, Identifiable {
     
     //신청자 서브컬렉션 추가
     var applicantsCollectionRef: CollectionReference {
-        return Firestore.firestore().collection("Recruit").document(fieldID).collection("Applicants")
+        return REF_RECRUIT.document(fieldID).collection("Applicants")
     }
 
     //서브콜렉션에 신청자 추가
     func apply(withUserID userID: String?) {
         applicantsCollectionRef.document(userID ?? "").setData(["userID": userID])
+    }
+    
+    //수락하기 하면 배열에 Recruit의 추가
+    func acceptedApplicants(withUserID userID: String, toNewDocument newDocumentID: String) {
+        let moveToDoc = REF_RECRUIT.document(fieldID)
+            
+        moveToDoc.getDocument { snapshot, error in
+            if let error {
+                print("승인된 유저 UID 이동 오류")
+            }
+            
+            if var data = snapshot?.data(), var acceptedApplicants = data["acceptedApplicants"] as? [String] {
+                // 이미 존재하는 배열에 새로운 유저 아이디 추가
+                acceptedApplicants.append(userID)
+                // 업데이트된 배열을 다시 Firestore에 저장
+                data["acceptedApplicants"] = acceptedApplicants
+                moveToDoc.setData(data)
+            } else {
+                // 배열이 없으면 새로 생성하고 유저 아이디 추가
+                let newAcceptedApplicants = [userID]
+                moveToDoc.setData(["acceptedApplicants": newAcceptedApplicants])
+            }
+        }
+    }
+    
+    //거절하기 하면 uid 지움
+    func deleteApplicant(withUserID userID: String) {
+        applicantsCollectionRef.document(userID).delete { error in
+            if let error = error {
+                print("Error deleting document: \(error)")
+            } else {
+                print("Document successfully deleted!")
+            }
+        }
     }
 
     //서브콜렉션에서 신청자 제거
