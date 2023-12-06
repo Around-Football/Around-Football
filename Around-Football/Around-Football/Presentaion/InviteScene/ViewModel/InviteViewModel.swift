ed54912ba557116a75c44e6b1ebc5ab2f3a1a826
thekoon0456 <thekoon0456@gmail.com>
2023-12-06 22:18:00 +0900

    [Feat] 용병구하기 TextField Rx구현

--- Around-Football/Around-Football/Presentaion/InviteScene/ViewModel/InviteViewModel.swift
+++ Around-Football/Around-Football/Presentaion/InviteScene/ViewModel/InviteViewModel.swift
@@ -18,14 +18,20 @@ final class InviteViewModel {
     private var disposeBag = DisposeBag()
     
     private var fieldID = UUID().uuidString
-    var fieldName: BehaviorRelay<String?> = BehaviorRelay(value: "")
-    var fieldAddress: BehaviorRelay<String?> = BehaviorRelay(value: "")
-    var region: BehaviorRelay<String?> = BehaviorRelay(value: "")
-    var type: BehaviorRelay<String?> = BehaviorRelay(value: "")
-    var gamePrice: BehaviorRelay<String?> = BehaviorRelay(value: "")
+    var fieldName: BehaviorRelay<String?> = BehaviorRelay(value: nil)
+    var fieldAddress: BehaviorRelay<String?> = BehaviorRelay(value: nil)
+    var region: BehaviorRelay<String?> = BehaviorRelay(value: nil)
+    var type: BehaviorRelay<String?> = BehaviorRelay(value: nil)
+    var gamePrice: BehaviorRelay<String?> = BehaviorRelay(value: nil)
+    var peopleCount: BehaviorRelay<Int> = BehaviorRelay(value: 1)
+    var contentTitle: BehaviorRelay<String?> = BehaviorRelay(value: nil)
+    var content: BehaviorRelay<String?> = BehaviorRelay(value: nil)
+    var matchDateString: BehaviorRelay<String?> = BehaviorRelay(value: nil)
+    var startTime: BehaviorRelay<String?> = BehaviorRelay(value: nil)
+    var endTime: BehaviorRelay<String?> = BehaviorRelay(value: nil)
     private let pendingApplicantsUID: [String?] = []
     private let acceptedApplicantsUID: [String?] = []
-
+    
     var coordinator: InviteCoordinator
     
     init(coordinator: InviteCoordinator) {
@@ -43,12 +49,7 @@ final class InviteViewModel {
         .disposed(by: disposeBag)
     }
     
-    func createRecruitFieldData(peopleCount: Int,
-                                contentTitle: String?,
-                                content: String?,
-                                matchDateString: String?,
-                                startTime: String?,
-                                endTime: String?) {
+    func createRecruitFieldData() {
         // MARK: - 테스트용 임시 데이터 파베에 올림
         FirebaseAPI.shared.createRecruitFieldData(user: user,
                                                   fieldID: fieldID,
@@ -56,13 +57,13 @@ final class InviteViewModel {
                                                   fieldAddress: fieldAddress.value ?? "",
                                                   region: region.value ?? "",
                                                   type: type.value ?? "",
-                                                  recruitedPeopleCount: peopleCount,
+                                                  recruitedPeopleCount: peopleCount.value,
                                                   gamePrice: gamePrice.value ?? "",
-                                                  title: contentTitle,
-                                                  content: content,
-                                                  matchDateString: matchDateString,
-                                                  startTime: startTime,
-                                                  endTime: endTime,
+                                                  title: contentTitle.value ?? "",
+                                                  content: content.value ?? "",
+                                                  matchDateString: matchDateString.value ?? "",
+                                                  startTime: startTime.value ?? "",
+                                                  endTime: endTime.value ?? "",
                                                   pendingApplicantsUID: pendingApplicantsUID,
                                                   acceptedApplicantsUID: acceptedApplicantsUID) { error in
             if error == nil {
