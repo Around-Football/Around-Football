cfce76819db2aabc941c3dd157e3b13948a03a76
thekoon0456 <thekoon0456@gmail.com>
2023-12-10 22:15:19 +0900

    [Feat] Info뷰 관심 글, 작성 글, 신청 글 세가지 카테고리로 만들고 뷰컨 연결

---
+++ Around-Football/Around-Football/Presentaion/MainTabBarScene/InfoTab/View/InfoDetailView/BookmarkPostViewController.swift
@@ -0,0 +1,17 @@
+//
+//  BookmarkPostViewController.swift
+//  Around-Football
+//
+//  Created by Deokhun KIM on 12/10/23.
+//
+
+import UIKit
+
+class BookmarkPostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+}
---
+++ Around-Football/Around-Football/Presentaion/MainTabBarScene/InfoTab/View/InfoDetailView/WrittenPostViewController.swift
@@ -0,0 +1,17 @@
+//
+//  WrittenPostViewController.swift
+//  Around-Football
+//
+//  Created by Deokhun KIM on 12/10/23.
+//
+
+import UIKit
+
+class WrittenPostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+    
+}
