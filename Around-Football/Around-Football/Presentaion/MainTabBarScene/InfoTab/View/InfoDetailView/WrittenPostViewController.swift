b90fca532e59c9cd9d0d0d22edcfdccafe1e3330
thekoon0456 <thekoon0456@gmail.com>
2023-12-11 17:48:56 +0900

    [Feat] 세뷰 뷰 뷰컨트롤러 생성

--- Around-Football/Around-Football/Presentaion/MainTabBarScene/InfoTab/View/InfoDetailView/WrittenPostViewController.swift
+++ Around-Football/Around-Football/Presentaion/MainTabBarScene/InfoTab/View/InfoDetailView/WrittenPostViewController.swift
@@ -7,7 +7,23 @@
 
 import UIKit
 
+import RxCocoa
+import RxSwift
+import SnapKit
+import Then
+
 class WrittenPostViewController: UIViewController {
+    
+    var viewModel: InfoPostViewModel?
+    
+    init(viewModel: InfoPostViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
