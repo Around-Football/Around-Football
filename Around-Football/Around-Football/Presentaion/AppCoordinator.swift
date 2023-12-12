bf804607dcb88580919743903b17c58fc080938b
thekoon0456 <thekoon0456@gmail.com>
2023-12-13 00:40:05 +0900

    [Feat] AppCoordinator에서 딥링크 타입 추가하고 기존 메서드 삭제

--- Around-Football/Around-Football/Presentaion/AppCoordinator.swift
+++ Around-Football/Around-Football/Presentaion/AppCoordinator.swift
@@ -24,6 +24,7 @@ enum CoordinatorType {
     case chat
     case info
     case detailScene
+    case deepLink
 }
 
 protocol Coordinator: AnyObject {
@@ -73,17 +74,11 @@ final class AppCoordinator: BaseCoordinator, LoginCoordinatorDelegate, MainTabBa
     }
     
     //TODO: - 온보딩뷰 컨트롤러로 변경
+    
     func presentLoginViewController() {
         let coordinator = LoginCoordinator(navigationController: navigationController)
         coordinator.delegate = self
         coordinator.start() //여기서 모달뷰로 만듬
         childCoordinators.append(coordinator)
-    }
-    
-    func handleChatDeepLink(channelInfo: ChannelInfo) {
-        
-        if let mainTabBarCoordinator = childCoordinators.first(where: { $0 is MainTabBarCoordinator }) as? MainTabBarCoordinator {
-            mainTabBarCoordinator.chatCoordinatorDeepLink(channelInfo: channelInfo)
-        }
     }
 }
