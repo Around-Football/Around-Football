d3810f72b0b6884226af5be53d509d7c86560757
Changhyun Kang <changhyun.kyle@gmail.com>
2024-01-03 21:35:56 +0900

    [Add] InviteImageView 추가

--- Around-Football/Around-Football/Presentaion/InviteScene/View/InviteImageView.swift
+++ Around-Football/Around-Football/Presentaion/InviteScene/View/InviteImageView.swift
@@ -10,17 +10,27 @@ import UIKit
 import SnapKit
 import Then
 
-class InviteImageView: UIView {
-    
-    var inviteImage
+class InviteImageView: UIImageView {
+    private let deleteButton = UIButton().then {
+        $0.setImage(UIImage(named: AFIcon.deleteButton), for: .normal)
+    }
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        configureUI()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    
+    private func configureUI() {
+        addSubview(deleteButton)
+        deleteButton.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(10)
+            make.trailing.equalToSuperview().offset(-10)
+            make.width.equalTo(10)
+            make.height.equalTo(10)
+        }
+    }
 }
