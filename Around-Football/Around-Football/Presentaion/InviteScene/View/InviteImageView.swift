7e7e8775c85009921c519fc1142bbac3c8b92aa0
Changhyun Kang <changhyun.kyle@gmail.com>
2024-01-03 21:33:04 +0900

    [Feat] InviteView -> showPHPickerView() 구현

---
+++ Around-Football/Around-Football/Presentaion/InviteScene/View/InviteImageView.swift
@@ -0,0 +1,26 @@
+//
+//  ImageView.swift
+//  Around-Football
+//
+//  Created by 강창현 on 1/3/24.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+class InviteImageView: UIView {
+    
+    var inviteImage
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+}
