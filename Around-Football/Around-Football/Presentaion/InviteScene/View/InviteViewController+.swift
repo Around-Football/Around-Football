697e73e6e57afc8671bfe692e4ab43dcb3026207
Changhyun Kang <changhyun.kyle@gmail.com>
2024-01-03 21:34:56 +0900

    [Refactor] 불필요 함수 제거

--- Around-Football/Around-Football/Presentaion/InviteScene/View/InviteViewController+.swift
+++ Around-Football/Around-Football/Presentaion/InviteScene/View/InviteViewController+.swift
@@ -33,23 +33,6 @@ extension InviteViewController {
 
 // MARK: - TextViewDelegate
 
-extension InviteViewController: UITextFieldDelegate {
-    
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        guard let text = (textField.text as NSString?)?.replacingCharacters(in: range, with: string) else { return true }
-
-//        if text.isEmpty {
-//            titlePlaceHolderLabel.isHidden = false
-//            viewModel.contentTitle.accept(nil)
-//        } else {
-//            titlePlaceHolderLabel.isHidden = true
-//            viewModel.contentTitle.accept(text)
-//        }
-        
-        return true
-    }
-}
-
 extension InviteViewController: UITextViewDelegate {
     func textViewDidChange(_ textView: UITextView) {
         if textView.text.isEmpty {
