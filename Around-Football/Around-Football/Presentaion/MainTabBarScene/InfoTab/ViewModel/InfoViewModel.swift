630a9da5c8dd07757aa44e235f857d3c636a49a7
Deokhun KIM <thekoon0456@gmail.com>
2024-01-07 22:52:58 +0900

    [Feat] 세팅뷰 북마크cell 구현

--- Around-Football/Around-Football/Presentaion/MainTabBarScene/InfoTab/ViewModel/InfoViewModel.swift
+++ Around-Football/Around-Football/Presentaion/MainTabBarScene/InfoTab/ViewModel/InfoViewModel.swift
@@ -17,12 +17,10 @@ final class InfoViewModel {
     var user: User?
     private let menus = ["내 정보 수정", "관심 글", "신청 글", "작성 글"]
     lazy var menusObservable = Observable.just(menus)
-
+    
     // MARK: - Lifecycles
     
     init(coordinator: InfoTabCoordinator) {
         self.coordinator = coordinator
     }
-    
-
 }
