3371efe2fbf5635daf765c8108431b87f43ecd2a
Changhyun Kang <changhyun.kyle@gmail.com>
2024-01-15 19:57:14 +0900

    [Refactor] FieldDetailViewModel 제거

--- Around-Football/Around-Football/Presentaion/MainTabBarScene/MapTab/ViewModel/FieldDetailViewModel.swift
+++ Around-Football/Around-Football/Presentaion/MainTabBarScene/MapTab/ViewModel/FieldDetailViewModel.swift
@@ -11,24 +11,24 @@ final class FieldDetailViewModel {
     
     // MARK: - Properties
     
-    let field: Field
+//    let field: Field
     private var recruits: [Recruit] = []
     private let firebaseAPI = FirebaseAPI.shared
 
     // MARK: - Lifecycles
     
-    init(field: Field) {
-        self.field = field
-    }
+//    init(field: Field) {
+//        self.field = field
+//    }
     
     // MARK: - API
     
-    func fetchRecruitFieldData(date: Date, completion: @escaping(([Recruit]) -> Void)) {
-        firebaseAPI.fetchRecruitFieldData(fieldID: field.id, date: date) { recruits in
-            //TODO: -Date sort
-//            self.recruits = recruits.sorted(by: { $0.matchDateString ?? "0" < $1.matchDateString ?? "1" })
-            
-            completion(recruits)
-        }
-    }
+//    func fetchRecruitFieldData(date: Date, completion: @escaping(([Recruit]) -> Void)) {
+//        firebaseAPI.fetchRecruitFieldData(fieldID: field.id, date: date) { recruits in
+//            //TODO: -Date sort
+////            self.recruits = recruits.sorted(by: { $0.matchDateString ?? "0" < $1.matchDateString ?? "1" })
+//            
+//            completion(recruits)
+//        }
+//    }
 }
