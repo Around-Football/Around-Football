b414d706210c3827327934c84ce33eb2eaa4472a
Deokhun KIM <thekoon0456@gmail.com>
2024-01-17 21:35:23 +0900

    [Feat] 디자인 수정

--- Around-Football/Around-Football/Presentaion/InviteScene/View/InviteViewController.swift
+++ Around-Football/Around-Football/Presentaion/InviteScene/View/InviteViewController.swift
@@ -61,6 +61,7 @@ final class InviteViewController: UIViewController, Searchable {
         $0.preferredDatePickerStyle = .compact
         $0.datePickerMode = .date
         $0.minimumDate = Date()
+        $0.tintColor = AFColor.futsal
         $0.locale = Locale(identifier: "ko_KR")
         $0.layer.cornerRadius = 8
         $0.clipsToBounds = true
