a945e3d97ff53b05b733696dced9bbdfe6cad7e6
Changhyun Kang <changhyun.kyle@gmail.com>
2024-01-21 17:27:24 +0900

    [Refactor] FieldModel 구조 변경

--- Around-Football/Around-Football/Models/FieldModel.swift
+++ Around-Football/Around-Football/Models/FieldModel.swift
@@ -15,24 +15,25 @@ struct Field: Codable, Identifiable {
     var id: String //운동장ID(추가)
     var fieldName: String //운동장 이름
     var fieldAddress: String //운동장 주소
-//    var imageURL: String //운동장 이미지
     var location: GeoPoint //운동장 좌표
+    var data: [String:[Recruit]]
     
-    static func convertToArray(documents: [[String: Any]]) -> [Field] {
-        var array: [Field] = []
-        for document in documents {
-            let field = Field(dictionary: document)
-            array.append(field)
-        }
-        
-        return array
+    var representation: [String: Any] {
+        let rep = ["id": id,
+                   "fieldName": fieldName,
+                   "fieldAddress": fieldAddress,
+                   "location": location,
+                   "data": data
+                   ] as [String : Any]
+        return rep
     }
     
-    init(dictionary: [String: Any]) {
-        self.id = dictionary["id"] as? String ?? UUID().uuidString
-        self.fieldName = dictionary["fieldName"] as? String ?? ""
-        self.fieldAddress = dictionary["fieldAddress"] as? String ?? ""
-//        self.imageURL = dictionary["imageURL"] as? String ?? ""
-        self.location = dictionary["location"] as? GeoPoint ?? GeoPoint(latitude: 0, longitude: 0)
+    init(id: String, fieldName: String, fieldAddress: String, location: GeoPoint, data: [String:[Recruit]] ) {
+        self.id = id
+        self.fieldName = fieldName
+        self.fieldAddress = fieldAddress
+        self.location = location
+        self.data = data
     }
 }
+
