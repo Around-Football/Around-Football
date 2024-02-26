bf6078e23a62448d2e22903cd7599828723e84cb
Changhyun Kang <changhyun.kyle@gmail.com>
2024-02-26 16:09:40 +0900

    [Feat] FieldDetailViewModel 데이터 프로퍼티 구현

--- Around-Football/Around-Football/Presentaion/MainTabBarScene/MapTab/ViewModel/FieldDetailViewModel.swift
+++ Around-Football/Around-Football/Presentaion/MainTabBarScene/MapTab/ViewModel/FieldDetailViewModel.swift
@@ -7,26 +7,41 @@
 
 import Foundation
 
+import RxSwift
+
 final class FieldDetailViewModel {
     
     // MARK: - Properties
     
-    private let field: Field
-    private var recruits: [Recruit] = []
+//    private let field: Field
+    private var recruit: Recruit
     private let firebaseAPI = FirebaseAPI.shared
+    var playTime: String {
+        let startTime = self.recruit.startTime
+        let endTime = self.recruit.endTime
+        return startTime + "-" + endTime
+    }
+    var fieldName: String {
+        return recruit.fieldName
+    }
+    var addressLabel: String {
+        return recruit.fieldAddress
+    }
+    var recruitNumber: String {
+        let number = self.recruit.recruitedPeopleCount
+        return "0 / \(number)"
+    }
 
     // MARK: - Lifecycles
-    
-    init(field: Field) {
-        self.field = field
+
+    init(recruit: Recruit) {
+        self.recruit = recruit
     }
-    
     // MARK: - API
-    
-    func fetchRecruitFieldData(completion: @escaping(([Recruit]) -> Void)) {
-        firebaseAPI.fetchRecruitFieldData(fieldID: field.id) { recruits in
+//    func fetchRecruitFieldData(completion: @escaping(([Recruit]) -> Void)) {
+//        firebaseAPI.fetchRecruitFieldData(fieldID: field.id) { recruits in
 //            self.recruits = recruits.sorted(by: { $0.matchDateString ?? "0" < $1.matchDateString ?? "1" })
-            completion(recruits)
-        }
-    }
+//            completion(recruits)
+//        }
+//    }
 }
