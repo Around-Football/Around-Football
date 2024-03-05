85da02cbf949a94e99d9b82e829fc0b9d5e3fbdb
Changhyun Kang <changhyun.kyle@gmail.com>
2024-03-05 22:24:56 +0900

    [Remove] RxSwift 적용으로 인한 파일 삭제

--- Around-Football/Around-Football/Presentaion/MainTabBarScene/MapTab/View/FieldDetailViewController+.swift
+++ Around-Football/Around-Football/Presentaion/MainTabBarScene/MapTab/View/FieldDetailViewController+.swift
@@ -5,38 +5,44 @@
 //  Created by 진태영 on 10/23/23.
 //
 
-import UIKit
+// TODO: - Rx 적용 -> 삭제
 
-extension FieldDetailViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return viewModel.recruitsCount
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard
-            let cell = tableView.dequeueReusableCell(
-                withIdentifier: FieldRecruitTableViewCell.identifier,
-                for: indexPath
-            ) as? FieldRecruitTableViewCell
-        else {
-            return FieldRecruitTableViewCell()
-        }
-        let recruit = viewModel.fetchRecruit(row: indexPath.row)
-        cell.configure(recruit: recruit)
-        cell.checkMyRecruit(result: viewModel.checkMyRecruit(recruit: recruit))
-        cell.bindButton(disposeBag: disposeBag) {
-            self.viewModel.checkChannelAndPushChatViewController(recruit: recruit)
-        }
-        return cell
-    }
-}
+//import UIKit
 
-extension FieldDetailViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        // TODO: - dismiss도 coordinator 통해서? -> 그러면 FieldDetailViewCoordinator 있어야할 듯?
-        self.dismiss(animated: true) {
-            let recruit = self.viewModel.fetchRecruit(row: indexPath.row)
-            self.viewModel.pushRecruitDetailView(recruit: recruit)
-        }
-    }
-}
+//extension FieldDetailViewController: UITableViewDataSource {
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return viewModel.recruitsCount
+//    }
+//    
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        guard
+//            let cell = tableView.dequeueReusableCell(
+//                withIdentifier: FieldRecruitTableViewCell.identifier,
+//                for: indexPath
+//            ) as? FieldRecruitTableViewCell
+//        else {
+//            return FieldRecruitTableViewCell()
+//        }
+//        let recruit = viewModel.fetchRecruit(row: indexPath.row)
+//        cell.configure(recruit: recruit)
+//        cell.checkMyRecruit(result: viewModel.checkMyRecruit(recruit: recruit))
+//        cell.bindButton(disposeBag: disposeBag) {
+//            self.viewModel.checkChannelAndPushChatViewController(recruit: recruit)
+//        }
+//        return cell
+//    }
+//}
+
+//extension FieldDetailViewController: UITableViewDelegate {
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        // TODO: - dismiss도 coordinator 통해서? -> 그러면 FieldDetailViewCoordinator 있어야할 듯?
+//        self.dismiss(animated: true) {
+//            guard 
+//                let recruit = self.viewModel.fetchRecruit(row: indexPath.row)
+//            else {
+//                return
+//            }
+//            self.viewModel.pushRecruitDetailView(recruit: recruit)
+//        }
+//    }
+//}
