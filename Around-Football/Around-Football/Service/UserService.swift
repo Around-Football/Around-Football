85c56f77804ccc9fd712a3df508ce62272244865
Changhyun Kang <changhyun.kyle@gmail.com>
2024-03-19 17:26:21 +0900

    [Chore] 코드 컨벤션에 맞게 수정

--- Around-Football/Around-Football/Service/UserService.swift
+++ Around-Football/Around-Football/Service/UserService.swift
@@ -24,7 +24,8 @@ final class UserService: NSObject {
     // MARK: - Properties
     
     static let shared = UserService()
-    var currentNonce: String?
+    private let disposeBag = DisposeBag()
+    private var currentNonce: String?
     
     // MARK: - Rx Properties
     
@@ -33,7 +34,6 @@ final class UserService: NSObject {
     var isLogoutObservable: PublishSubject<Void> = PublishSubject()
     var checkUserInfoExist: PublishSubject<Bool> = PublishSubject()
     var isLoginProcess: PublishSubject<Bool> = PublishSubject()
-    private let disposeBag = DisposeBag()
     
     // MARK: - Lifecycles
     
@@ -43,73 +43,15 @@ final class UserService: NSObject {
         configureLogOutObserver()
     }
     
-    private func configureCurrentUser() {
-        isLoginObservable
-            .withUnretained(self)
-            .flatMap { (owner, _) -> Observable<User?> in
-                guard let uid = Auth.auth().currentUser?.uid else { return .empty() }
-                let fcmToken = UserDefaults.standard.string(forKey: "FCMToken") ?? ""
-                print("DEBUG - Set FCMToken: \(fcmToken)")
-                return FirebaseAPI.shared.updateFCMTokenAndFetchUser(uid: uid, fcmToken: fcmToken)
-                    .asObservable()
-                    .catchAndReturn(nil)
-            }
-            .subscribe(onNext: { [weak self] user in
-                guard let self else { return }
-                currentUser_Rx.onNext(user)
-                checkUserInfoExist.onNext(true)
-                NotiManager.shared.setAppIconBadgeNumber(number: user?.totalAlarmNumber ?? 0)
-                isLoginProcess.onNext(false)
-            })
-            .disposed(by: disposeBag)
+    func kakaoSignIn() {
+        let isInstalledKakaoApp = UserApi.isKakaoTalkLoginAvailable()
+        // 카카오톡 설치 여부 확인
+        if isInstalledKakaoApp {
+            loginKakaoApp()
+        } else {
+            loginKakaoAccount()
+        }
     }
-    
-    private func configureLogOutObserver() {
-        isLogoutObservable
-            .withUnretained(self)
-            .subscribe { (owner, _) in
-                print("logout 1")
-                guard let uid = try? owner.currentUser_Rx.value()?.id else { return }
-                print("logout 2")
-                // MARK: - 로그아웃시 nil보냄
-                owner.currentUser_Rx.onNext(nil)
-                print("logout 3")
-                FirebaseAPI.shared.updateFCMToken(uid: uid, fcmToken: "") { error in
-                    print("logout 4")
-                    print("DEBUG - update logout fcm token")
-                    if let error = error {
-                        print("logout 5")
-                        print("DEBUG - Logout FCM update error", error.localizedDescription as Any)
-                        return
-                    }
-                    print("logout 6")
-                }
-                
-                Messaging.messaging().deleteToken { error in
-                    if let error = error {
-                        print("DEBUG - deleteToken Error: \(error.localizedDescription)")
-                    } else {
-                        Messaging.messaging().token { token, _ in
-                            if let token = token {
-                                print("DEBUG - FCM 토큰", #function, token)
-                                UserDefaults.standard.set(token, forKey: "FCMToken")
-                            }
-                        }
-                    }
-                }
-                
-                // Client에 전송된, 전송될 알림 모두 제거
-                let center = UNUserNotificationCenter.current()
-                center.removeAllPendingNotificationRequests()
-                center.removeAllDeliveredNotifications()
-                
-                NotiManager.shared.setAppIconBadgeNumber(number: 0)
-            }
-            .disposed(by: disposeBag)
-    }
-    
-    
-    // MARK: - Helpers - Google
     
     func googleSignIn(_ controller: UIViewController) {
         
@@ -158,7 +100,6 @@ final class UserService: NSObject {
                                     print("setUserDataError: \(String(describing: error?.localizedDescription))")
                                     return
                                 }
-                                
                                 isLoginObservable.onNext(())
                             }
                     }
@@ -168,18 +109,165 @@ final class UserService: NSObject {
         }
     }
     
-    // MARK: - Helpers - Kakao
+    func appleSignIn() {
+        let nonce = randomNonceString()
+        currentNonce = nonce
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        let request = appleIDProvider.createRequest()
+        request.requestedScopes = [.fullName, .email]
+        request.nonce = sha256(nonce)
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        authorizationController.delegate = self
+        authorizationController.performRequests()
+        print("애플로그인 진행")
+    }
     
-    func kakaoSignIn() {
-        let isInstalledKakaoApp = UserApi.isKakaoTalkLoginAvailable()
-        // 카카오톡 설치 여부 확인
-        if isInstalledKakaoApp {
-            loginKakaoApp()
-        } else {
-            loginKakaoAccount()
+    func logout() {
+        let firebaseAuth = Auth.auth()
+        do {
+            try firebaseAuth.signOut()
+            isLogoutObservable.onNext(())
+            print("userLogout")
+        } catch let signOutError as NSError {
+            print("Error signing out: %@", signOutError)
         }
     }
     
+    func deleteUser() {
+        guard let user = Auth.auth().currentUser else {
+            // 유저가 현재 로그인되어 있지 않은 경우
+            print("No user is currently signed in.")
+            return
+        }
+        
+        user.delete { error in
+            if let error = error {
+                print("Error deleting user: \(error.localizedDescription)")
+            } else {
+                print("User deleted successfully.")
+                //파베 유저 삭제
+                FirebaseAPI.shared.deleteUser(user.uid)
+            }
+        }
+    }
+}
+
+// MARK: - Helpers - Rx
+
+private extension UserService {
+    func configureCurrentUser() {
+        isLoginObservable
+            .withUnretained(self)
+            .flatMap { (owner, _) -> Observable<User?> in
+                guard let uid = Auth.auth().currentUser?.uid else { return .empty() }
+                let fcmToken = UserDefaults.standard.string(forKey: "FCMToken") ?? ""
+                print("DEBUG - Set FCMToken: \(fcmToken)")
+                return FirebaseAPI.shared.updateFCMTokenAndFetchUser(uid: uid, fcmToken: fcmToken)
+                    .asObservable()
+                    .catchAndReturn(nil)
+            }
+            .subscribe(onNext: { [weak self] user in
+                guard let self else { return }
+                currentUser_Rx.onNext(user)
+                checkUserInfoExist.onNext(true)
+                NotiManager.shared.setAppIconBadgeNumber(number: user?.totalAlarmNumber ?? 0)
+                isLoginProcess.onNext(false)
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    func configureLogOutObserver() {
+        isLogoutObservable
+            .withUnretained(self)
+            .subscribe { (owner, _) in
+                print("logout 1")
+                guard let uid = try? owner.currentUser_Rx.value()?.id else { return }
+                print("logout 2")
+                // MARK: - 로그아웃시 nil보냄
+                owner.currentUser_Rx.onNext(nil)
+                print("logout 3")
+                FirebaseAPI.shared.updateFCMToken(uid: uid, fcmToken: "") { error in
+                    print("logout 4")
+                    print("DEBUG - update logout fcm token")
+                    if let error = error {
+                        print("logout 5")
+                        print("DEBUG - Logout FCM update error", error.localizedDescription as Any)
+                        return
+                    }
+                    print("logout 6")
+                }
+                
+                Messaging.messaging().deleteToken { error in
+                    if let error = error {
+                        print("DEBUG - deleteToken Error: \(error.localizedDescription)")
+                    } else {
+                        Messaging.messaging().token { token, _ in
+                            if let token = token {
+                                print("DEBUG - FCM 토큰", #function, token)
+                                UserDefaults.standard.set(token, forKey: "FCMToken")
+                            }
+                        }
+                    }
+                }
+                // Client에 전송된, 전송될 알림 모두 제거
+                let center = UNUserNotificationCenter.current()
+                center.removeAllPendingNotificationRequests()
+                center.removeAllDeliveredNotifications()
+                
+                NotiManager.shared.setAppIconBadgeNumber(number: 0)
+            }
+            .disposed(by: disposeBag)
+    }
+}
+
+// MARK: - Helpers - Apple
+
+private extension UserService {
+    func sha256(_ input: String) -> String {
+        let inputData = Data(input.utf8)
+        let hashedData = SHA256.hash(data: inputData)
+        let hashString = hashedData.compactMap {
+            return String(format: "%02x", $0)
+        }.joined()
+        
+        return hashString
+    }
+    
+    func randomNonceString(length: Int = 32) -> String {
+        precondition(length > 0)
+        let charset: Array<Character> =
+        Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+        var result = ""
+        var remainingLength = length
+        
+        while remainingLength > 0 {
+            let randoms: [UInt8] = (0 ..< 16).map { _ in
+                var random: UInt8 = 0
+                let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
+                if errorCode != errSecSuccess {
+                    fatalError("Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)")
+                }
+                return random
+            }
+            
+            randoms.forEach { random in
+                if remainingLength == 0 {
+                    return
+                }
+                
+                if random < charset.count {
+                    result.append(charset[Int(random)])
+                    remainingLength -= 1
+                }
+            }
+        }
+        return result
+    }
+}
+
+// MARK: - Helpers - Kakao
+
+private extension UserService {
     func loginKakaoApp() {
         UserApi.shared.loginWithKakaoTalk { [weak self] (oauthToken, error) in
             self?.isLoginProcess.onNext(true)
@@ -238,92 +326,6 @@ final class UserService: NSObject {
             self.googleLogOut()
             self.isLogoutObservable.onNext(())
             print("logout() success.")
-        }
-    }
-    
-    // MARK: - Helpers - Apple
-    
-    func appleSignIn() {
-        let nonce = randomNonceString()
-        currentNonce = nonce
-        let appleIDProvider = ASAuthorizationAppleIDProvider()
-        let request = appleIDProvider.createRequest()
-        request.requestedScopes = [.fullName, .email]
-        request.nonce = sha256(nonce)
-        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
-        authorizationController.delegate = self
-        authorizationController.performRequests()
-        print("애플로그인 진행")
-    }
-    
-    private func sha256(_ input: String) -> String {
-        let inputData = Data(input.utf8)
-        let hashedData = SHA256.hash(data: inputData)
-        let hashString = hashedData.compactMap {
-            return String(format: "%02x", $0)
-        }.joined()
-        
-        return hashString
-    }
-    
-    private func randomNonceString(length: Int = 32) -> String {
-        precondition(length > 0)
-        let charset: Array<Character> =
-        Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
-        var result = ""
-        var remainingLength = length
-        
-        while remainingLength > 0 {
-            let randoms: [UInt8] = (0 ..< 16).map { _ in
-                var random: UInt8 = 0
-                let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
-                if errorCode != errSecSuccess {
-                    fatalError("Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)")
-                }
-                return random
-            }
-            
-            randoms.forEach { random in
-                if remainingLength == 0 {
-                    return
-                }
-                
-                if random < charset.count {
-                    result.append(charset[Int(random)])
-                    remainingLength -= 1
-                }
-            }
-        }
-        
-        return result
-    }
-    
-    func logout() {
-        let firebaseAuth = Auth.auth()
-        do {
-            try firebaseAuth.signOut()
-            isLogoutObservable.onNext(())
-            print("userLogout")
-        } catch let signOutError as NSError {
-            print("Error signing out: %@", signOutError)
-        }
-    }
-    
-    func deleteUser() {
-        guard let user = Auth.auth().currentUser else {
-            // 유저가 현재 로그인되어 있지 않은 경우
-            print("No user is currently signed in.")
-            return
-        }
-        
-        user.delete { error in
-            if let error = error {
-                print("Error deleting user: \(error.localizedDescription)")
-            } else {
-                print("User deleted successfully.")
-                //파베 유저 삭제
-                FirebaseAPI.shared.deleteUser(user.uid)
-            }
         }
     }
 }
@@ -399,8 +401,6 @@ extension UserService: ASAuthorizationControllerDelegate {
                     print ("Error Apple sign in: %@", error)
                     return
                 }
-                // User is signed in to Firebase with Apple.
-                // ...
                 print("로그인 성공")
                 
                 guard let uid = authResult?.user.uid else { return }
@@ -423,10 +423,6 @@ extension UserService: ASAuthorizationControllerDelegate {
                 }
             }
         }
-    }
-    
-    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
-        
     }
 }
 
