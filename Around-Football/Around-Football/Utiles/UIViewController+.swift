c4b7fea3d79fcce70bd36917fb6d5c634c28ec0c
Changhyun Kang <changhyun.kyle@gmail.com>
2024-05-16 15:03:30 +0900

    [Refactor] 이메일 보내기 기능 UIViewController+ Extension으로 분리

--- Around-Football/Around-Football/Utiles/UIViewController+.swift
+++ Around-Football/Around-Football/Utiles/UIViewController+.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import MessageUI
 
 extension UIViewController {
     func showPopUp(title: String? = nil,
@@ -82,5 +83,74 @@ extension UIViewController {
     @objc
     func dismissViewController() {
         dismiss(animated: false)
+    }
+}
+
+// MARK: - Action Sheet
+
+extension UIViewController {
+    func alertActionSheet(title: String? = nil, message: AlertMessage, actions: [UIAlertAction]) {
+        let actionSheet = UIAlertController(
+            title: title,
+            message: message.description,
+            preferredStyle: .actionSheet
+        )
+        for action in actions {
+            actionSheet.addAction(action)
+        }
+        
+        self.present(actionSheet, animated: true, completion: nil)
+    }
+}
+
+//MARK: - MessageUI 이메일
+
+extension UIViewController: MFMailComposeViewControllerDelegate {
+    func sendEmail(message: AlertMessage? = nil) {
+        if MFMailComposeViewController.canSendMail() {
+            let mailController = MFMailComposeViewController()
+            mailController.mailComposeDelegate = self
+            mailController.setToRecipients(["aroundfootball2024@gmail.com"]) // 수신자 이메일 주소
+            mailController.setSubject("[어라운드풋볼 1:1문의]") // 이메일 제목
+            guard let message else {
+                present(mailController, animated: true, completion: nil)
+                return
+            }
+            mailController.setMessageBody(message.description, isHTML: false) // 이메일 내용
+            
+            present(mailController, animated: true, completion: nil)
+        } else {
+            showSendMailErrorAlert()
+        }
+    }
+    
+    func showSendMailErrorAlert() {
+        let sendMailErrorAlert = UIAlertController(
+            title: "메일 전송 실패",
+            message: "이메일 설정을 확인하고 다시 시도해주세요.",
+            preferredStyle: .alert
+        )
+        
+        let confirmAction = UIAlertAction(title: "확인", style: .default) { action in print("확인") }
+        sendMailErrorAlert.addAction(confirmAction)
+        
+        self.present(sendMailErrorAlert, animated: true, completion: nil)
+    }
+    
+    public func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        switch result {
+        case .cancelled:
+            print("이메일 보내기 취소")
+        case .sent:
+            print("이메일 보내기 성공")
+        case .saved:
+            print("이메일이 저장되었습니다.")
+        case .failed:
+            print("이메일 보내기 실패")
+        @unknown default:
+            break
+        }
+        
+        controller.dismiss(animated: true, completion: nil)
     }
 }
